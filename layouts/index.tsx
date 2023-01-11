318da8546d3558bba426f19716d85bbfde54d185
thnhtus <fallforthat@gmail.com>
2023-01-11 23:42:31 +0700

    feat(app): add antd

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { ReactElement } from 'react'
+import { Layout, Menu } from 'antd'
 
+const { Header, Sider, Content } = Layout
 interface Props {
   children: ReactElement
 }
 
-const Layout: React.FC<Props> = ({ children }) => {
+const SideBar: React.FC<Props> = ({ children }) => {
   const router = useRouter()
 
   const menuItems = [
@@ -21,26 +23,48 @@ const Layout: React.FC<Props> = ({ children }) => {
   ]
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-purple-200 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase">
-        Next.js sidebar menu
-      </header>
-      <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-fuchsia-100 w-full md:w-60">
-          <nav>
-            <ul>
-              {menuItems.map(({ href, title }) => (
-                <li className="m-2" key={title}>
-                  <Link href={href}>{title}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </aside>
-        <main className="flex-1">{children}</main>
-      </div>
-    </div>
+    <Layout className="layout" style={{ height: '100vh' }}>
+      <Sider trigger={null}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              label: <Link href={'/'}>Home</Link>,
+            },
+            {
+              key: '2',
+              label: <Link href={'/about'}>About</Link>,
+            },
+            {
+              key: '3',
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        ></Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
 
-export default Layout
+export default SideBar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../layouts'
+import 'antd/dist/reset.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
