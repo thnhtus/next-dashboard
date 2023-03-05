ad710ab119f982ca7be5f626ddff7993fe0b72e5
thnhtus <fallforthat@gmail.com>
2023-03-05 23:25:05 +0700

    refactor(app): modify layouts

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd'
 import type { MenuProps } from 'antd'
 import {
@@ -23,12 +23,64 @@ interface Props {
   children: ReactElement
 }
 
+const menuItems1: MenuProps['items'] = [
+  {
+    key: '1',
+    icon: <HomeFilled />,
+    title: paths.dashboard.name,
+    label: <Link href={paths.dashboard.path}>{paths.dashboard.name}</Link>,
+  },
+  {
+    key: '2',
+    icon: <FundFilled />,
+    title: paths.transactions.name,
+    label: <Link href={paths.transactions.path}>{paths.transactions.name}</Link>,
+  },
+  {
+    key: '3',
+    icon: <BookFilled />,
+    title: paths.invoices.name,
+    label: <Link href={paths.invoices.path}>{paths.invoices.name}</Link>,
+  },
+  {
+    key: '4',
+    icon: <WalletFilled />,
+    title: paths.my_wallets.name,
+    label: <Link href={paths.my_wallets.path}>{paths.my_wallets.name}</Link>,
+  },
+  {
+    key: '5',
+    icon: <SettingFilled />,
+    title: paths.settings.name,
+    label: <Link href={paths.settings.path}>{paths.settings.name}</Link>,
+  },
+]
+
+const menuItems2: MenuProps['items'] = [
+  {
+    key: '6',
+    icon: <QuestionCircleFilled />,
+    title: paths.help.name,
+    label: <Link href={paths.help.path}>{paths.help.name}</Link>,
+  },
+  {
+    key: '7',
+    icon: <LogoutOutlined />,
+    title: paths.logout.name,
+    label: <Link href={paths.logout.path}>{paths.logout.name}</Link>,
+  },
+]
+
 const findPathName = (path: string) => {
-  const find = Object.values(paths).find(
-    (item: { name: string; path: string }) => item.path === path
-  )
+  const find = Object.values(paths).find((item) => item.path === path)
 
   return find?.name ?? ''
+}
+
+const findSelectedMenuKey = (path: string) => {
+  const find = Object.values(paths).find((item) => item.path === path)
+
+  return find?.key ?? ''
 }
 
 const SideBar: React.FC<Props> = ({ children }) => {
@@ -36,66 +88,40 @@ const SideBar: React.FC<Props> = ({ children }) => {
 
   const dispatch = useDispatch()
 
+  const [currentSelectedMenu, setCurrentSelectedMenu] = useState<string>()
+
   // console.log(router)
 
   useEffect(() => {
     if (router.pathname !== '/') {
       const pathName = findPathName(router.pathname)
       dispatch(setPathName({ path: pathName }))
+
+      setCurrentSelectedMenu(findSelectedMenuKey(router.pathname))
     }
   }, [dispatch, router])
 
   //-----------------------------------------------------------------------
 
-  const menuItems1: MenuProps['items'] = [
-    {
-      key: '1',
-      icon: <HomeFilled />,
-      label: <Link href={paths.dashboard.path}>{paths.dashboard.name}</Link>,
-    },
-    {
-      key: '2',
-      icon: <FundFilled />,
-      label: <Link href={paths.transactions.path}>{paths.transactions.name}</Link>,
-    },
-    {
-      key: '3',
-      icon: <BookFilled />,
-      label: <Link href={paths.invoices.path}>{paths.invoices.name}</Link>,
-    },
-    {
-      key: '4',
-      icon: <WalletFilled />,
-      label: <Link href={paths.my_wallets.path}>{paths.my_wallets.name}</Link>,
-    },
-    {
-      key: '5',
-      icon: <SettingFilled />,
-      label: <Link href={paths.settings.path}>{paths.settings.name}</Link>,
-    },
-  ]
-
-  const menuItems2: MenuProps['items'] = [
-    {
-      key: '6',
-      icon: <QuestionCircleFilled />,
-      label: <Link href={paths.help.path}>{paths.help.name}</Link>,
-    },
-    {
-      key: '7',
-      icon: <LogoutOutlined />,
-      label: <Link href={paths.logout.path}>{paths.logout.name}</Link>,
-    },
-  ]
   //-----------------------------------------------------------------------
 
   return (
     <Layout className={style.layout}>
       <Sider trigger={null} theme="light">
-        <div className="logo">LOGO.</div>
-        <div>
-          <Menu mode="inline" items={menuItems1} />
-          <Menu mode="inline" items={menuItems2} />
+        <div className={style.layout__logo}>LOGO.</div>
+        <div className={style.layout__menu}>
+          <Menu
+            mode="inline"
+            items={menuItems1}
+            style={{ border: 'none' }}
+            selectedKeys={[currentSelectedMenu ?? '']}
+          />
+          <Menu
+            mode="inline"
+            items={menuItems2}
+            style={{ border: 'none' }}
+            selectedKeys={[currentSelectedMenu ?? '']}
+          />
         </div>
       </Sider>
       <Layout>
--- routers/paths.ts
+++ routers/paths.ts
@@ -1,29 +1,36 @@
 const paths = {
   dashboard: {
+    key: '1',
     name: 'Dashboard',
     path: '/dashboard',
   },
   transactions: {
+    key: '2',
     name: 'Transactions',
     path: '/transactions',
   },
   invoices: {
+    key: '3',
     name: 'Invoices',
     path: '/invoices',
   },
   my_wallets: {
+    key: '4',
     name: 'My Wallets',
     path: '/my_wallets',
   },
   settings: {
+    key: '5',
     name: 'Settings',
     path: '/settings',
   },
   help: {
+    key: '6',
     name: 'Help',
     path: '/help',
   },
   logout: {
+    key: '7',
     name: 'Logout',
     path: '/logout',
   },
